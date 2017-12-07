8536f3c3088a8284fbf86922429a5850a2d3c4c2
jiben22 <benji.legallo22@live.fr>
2017-12-07 13:36:32 +0100

    Add Symfony Components

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return $this->render('OCPlatformBundle:Default:index.html.twig');
+    }
+}
