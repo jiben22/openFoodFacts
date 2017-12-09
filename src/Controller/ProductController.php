7ce15261881cbb074c511ec9f97e78fab97cbc41
jiben22 <benji.legallo22@live.fr>
2017-12-09 19:03:28 +0100

    Add function data_product()

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,39 @@
 // src/Controller/ProductController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-// N'oubliez pas ce use :
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Product;
 
 class ProductController extends Controller
 {
-    public function information()
+    public function information(Request $request)
     {
-        // renders templates/product_information/index.html
-        return $this->render('products/information.html.twig');
+        // We retrieve our id route parameter
+        $id = $request->attributes->get('id');
+        // We retrieve data product
+        $product = $this->data_product($id);
+
+        // renders templates/product_information/information.html.twig
+        return $this->render('products/information.html.twig', array(
+            'product' => $product
+        ));
+    }
+
+    public function data_product($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'Pas de produit pour cet id '.$id
+            );
+        }
+
+        return new JsonResponse($product);
     }
 }
