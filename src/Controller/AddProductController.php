07a77d2dfdd3e9be586b92bb4766b18d5d4294c5
jiben22 <benji.legallo22@live.fr>
2018-01-15 15:00:08 +0100

    Upgrade add product with a form not valid

--- src/Controller/AddProductController.php
+++ src/Controller/AddProductController.php
@@ -26,14 +26,36 @@ class AddProductController extends Controller
 
       $form->handleRequest($request);
 
+      if ($form->isSubmitted() && $form->isValid()) {
+
+          //Retrieve data form Field mapped Product
+          $data = $form->getData();
+          $product = $data["product"];
+          $additives = $data["additives"];
+          $brand = $data["brands"];
+          $country = $data["country"];
+          $ingredients = $data["ingredients"];
+          $nutritionalInformation = $data["nutritionalInformation"];
+
+          //TEST
+          //$product = $data["product"];
+          //var_dump($data);
+          //var_dump($product);
+          //var_dump($brand);
+
+          // We ADD Product in database
+          $this->addProductInDatabase($product, $additives, $brand, $country, $ingredients, $nutritionalInformation);
+
+          return $this->render('products/search.html.twig', array(
+        ));
+      }
 
       return $this->render('products/add.html.twig', array(
         'form' => $form->createView(),
     ));
   }
 
-  //Return the fields added in the form to search a product
-  //DEV
+  //Return the fields added in the form to add a product
   public function getFormAddProduct()
   {
 
@@ -59,7 +81,7 @@ class AddProductController extends Controller
           'required' => false,
         ))
 
-        ->add('serving_size', IntegerType::class, array(
+        ->add('ingredients_from_palm_oil_n', IntegerType::class, array(
           'label' => 'Nombre d\'ingrédients contenant de l\'huile de palme',
           'required' => false,
         ))
@@ -180,5 +202,101 @@ class AddProductController extends Controller
         ->getForm();
 
       return $form;
+  }
+
+  public function addProductInDatabase($product, $additive, $brand, $country, $ingredient, $nutritional_information)
+  {
+    /**
+     * Create new PRODUCT
+     * @var Product $product
+     */
+    //Entering data process for product_name (ucfirst)
+    $product_cap = strtolower($product->getProductName());
+    $product_cap = ucfirst($product_cap);
+    $product->setProductName($product_cap);
+    $product->setServingSize($product->'serving_size']);
+    $product->setAdditivesN($product['additives_n']);
+    $product->setIngredientsFromPalmOilN($product['ingredients_from_palm_oil_n']);
+    $product->setIngredientsThatMayBeFromPalmOilN($product['ingredients_that_may_be_from_palm_oil_n']);
+
+    $this->em->persist($product);
+
+
+    /**
+     * Create new ADDITIVES
+     * @var Additives $additive
+     */
+    $additive->setAdditiveFr($row['additives_fr']);
+
+    $this->em->persist($additive);
+    //Add the additive at list additives to product
+    $product->addAdditive($additive);
+
+
+    /**
+     * Create new BRANDS
+     * @var Brands $brand
+     */
+    $brand->setBrand($row['brands']);
+    $brand->setBrandTags($row['brands_tags']);
+
+    $this->em->persist($brand);
+
+    $product->setBrand($brand);
+
+
+    /**
+     * Create new COUNTRIES
+     * @var Countries $country
+     */
+    $country->setCountryFr($row['countries_fr']);
+
+    $this->em->persist($country);
+
+    $product->setCountry($country);
+
+
+    /**
+     * Create new INGREDIENTS
+     * @var Ingredients $ingredient
+     */
+    $ingredient->setIngredient($row['ingredients_text']);
+
+    $this->em->persist($ingredient);
+    //Add the ingredient at list ingredients to product
+    $product->addIngredient($ingredient);
+
+
+    /**
+     * Create new NUTRIONAL_INFORMATION
+     * @var NutritionalInformation $nutritional_information
+     */
+    $nutritional_information->setNutritionGradeFr($row['nutrition_grade_fr']);
+    $nutritional_information->setEnergy100g($row['energy_100g']);
+    $nutritional_information->setFat100g($row['fat_100g']);
+    $nutritional_information->setSaturatedFat100g($row['saturated-fat_100g']);
+    $nutritional_information->setCholesterol100g($row['cholesterol_100g']);
+    $nutritional_information->setCarbohydrates100g($row['carbohydrates_100g']);
+    $nutritional_information->setSugars100g($row['sugars_100g']);
+    $nutritional_information->setFiber100g($row['fiber_100g']);
+    $nutritional_information->setProteins100g($row['proteins_100g']);
+    $nutritional_information->setSalt100g($row['salt_100g']);
+    $nutritional_information->setSodium100g($row['sodium_100g']);
+    $nutritional_information->setVitaminA100g($row['vitamin-a_100g']);
+    $nutritional_information->setCalcium100g($row['calcium_100g']);
+    $nutritional_information->setIron100g($row['iron_100g']);
+
+    $this->em->persist($nutritional_information);
+
+    $product->setNutritionalInformation($nutritional_information);
+
+    //We flush the product with others entities
+    try {
+       // ...
+       $this->em->flush();
+    }
+      catch (UniqueConstraintViolationException $e) {
+        // ....
+    }
   }
 }
